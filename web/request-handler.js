29b108873178d3edba4879f1b3fb2d9a9c82b2ff
mishasaggi <monisha.saggi@gmail.com>
2015-08-19 19:59:08 -0500

    fetcher progress

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,7 +3,23 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 //require http-helpers
+//Posts from client
 
 exports.handleRequest = function (req, res) {
+  //request object, type GET
+  //pick up url, lookup url in paths.list and send that data
+
+  if(req.method === 'GET'){
+
+  }
+
   res.end(archive.paths.list);
 };
+
+
+// http.get(hostname, function(res){
+//   console.log("Status: "+ res.statusCode);
+
+// }).on('error', function(err){
+//   throw err;
+// });
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,53 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+//GET request
+var fs = require('fs');
+var path = require('path');
+var _ = require('underscore');
+var httpNode = require('http');
+var archiveHelpers = require('../helpers/archive-helpers');
+
+//create the difference array
+//content negotiation
+//path is default '\'
+//hostname is the target url
+
+var dummyList = ['www.google.com', 'www.yahoo.com', 'www.makersquare.com'];
+var siteData;
+
+var options = {
+  hostname: 'www.google.com',
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf8'
+  }
+}
+
+var req = http.request(options, function(res){
+  console.log("Status: "+ res.statusCode);
+  res.on('data', function(data){
+    siteData = data;
+  });
+
+});
+console.log(siteData, "data");
+req.on('error', function(err){
+  throw err;
+});
+
+// archiveHelpers.downloadUrl
+console.log(req, "request object");
+req.end();
+
+//open get request
+
+/*
+//calls the end automatically
+http.get(hostname, function(res){
+  console.log("Status: "+ res.statusCode);
+
+}).on('error', function(err){
+  throw err;
+});
+*/
