65dd9dcbf2ee05a6963fd4abb49b53661d1b7a2f
mishasaggi <monisha.saggi@gmail.com>
2015-08-20 18:04:14 -0500

    wip

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,20 +1,47 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var headers = require('./http-helpers');
 // require more modules/folders here!
 
 //require http-helpers
 //Posts from client
+var body = "";
+
 
 exports.handleRequest = function (req, res) {
   //request object, type GET
   //pick up url, lookup url in paths.list and send that data
 
-  if(req.method === 'GET'){
+  var statusCode = 200;
+  // headers.headers['Content-Type'] = "application/json";
+
+console.log("Serving request type " + req.method + " for url " + req.url);
+  if(req.method === 'POST'){
+    statusCode = 201;
+    req.on('data', function(data){
+      body += data;
+      console.log(body, "data as recieved by local server");
+    });
+
+    req.on('end', function(){
+      console.log(headers.headers, "headers in end");
+      res.writeHead(statusCode, headers.headers);
+    })
+    // res.writeHead(statusCode, headers.headers);
+    res.end('{}');
 
   }
 
+
+  if(req.method === 'OPTIONS'){
+    res.writeHead(201, headers.headers);
+    res.end('{}');
+  }
+  res.writeHead(statusCode, headers.headers)
   res.end(archive.paths.list);
 };
+
+
 
 
 // http.get(hostname, function(res){
@@ -23,3 +50,4 @@ exports.handleRequest = function (req, res) {
 // }).on('error', function(err){
 //   throw err;
 // });
+
